f5a548a0b6a0fb018c26a37ac40407d184cd3248
Conner Alegre <cga0@protonmail.com>
2016-08-03 00:26:46 -0600

    Changes to all in functionality didn't push

--- pokerOdds.js
+++ pokerOdds.js
@@ -1,18 +1,17 @@
 (() => {
-    const potOddsResult = document.getElementsByClassName('potOddsResult')[0];
-    const equityResult  = document.getElementsByClassName('equityResult')[0];
-    const goodBad       = document.getElementsByClassName('goodBad')[0];
-
     let calculateButton = document.getElementsByClassName('calculate')[0].addEventListener('click', () => {
-        const betAmount    = document.getElementsByClassName('betAmount')[0].value;
-        const potAmount    = document.getElementsByClassName('potAmount')[0].value;
-        const outsAmount   = parseFloat(document.getElementsByClassName('outsAmount')[0].value);
-        let allIn        = document.querySelector('input[name="allIn"]').checked;
-        const flopOrTurn   = document.querySelector('input[name="flopOrTurn"]:checked').value;
-        const potOdds      = ((betAmount / (parseFloat(betAmount) + parseFloat(potAmount))) * 100).toFixed(2);
-        const outsOddsFlop = allIn ? ((outsAmount / 47) * 100).toFixed(2) * 2 : (((outsAmount / 47) * 100).toFixed(2));
-        const outsOddsTurn = ((outsAmount / 46) * 100).toFixed(2);
-        const outsOdds     = flopOrTurn === 'Flop' ? outsOddsFlop : outsOddsTurn;
+        const betAmount     = document.getElementsByClassName('betAmount')[0].value;
+        const potAmount     = document.getElementsByClassName('potAmount')[0].value;
+        const outsAmount    = parseFloat(document.getElementsByClassName('outsAmount')[0].value);
+        const allIn         = document.querySelector('input[name="allIn"]').checked;
+        const potOddsResult = document.getElementsByClassName('potOddsResult')[0];
+        const equityResult  = document.getElementsByClassName('equityResult')[0];
+        const goodBad       = document.getElementsByClassName('goodBad')[0];
+        const flopOrTurn    = document.querySelector('input[name="flopOrTurn"]:checked').value;
+        const potOdds       = ((betAmount / (parseFloat(betAmount) + parseFloat(potAmount))) * 100).toFixed(2);
+        const outsOddsFlop  = allIn ? ((outsAmount / 47) * 100).toFixed(2) * 2 : (((outsAmount / 47) * 100).toFixed(2));
+        const outsOddsTurn  = ((outsAmount / 46) * 100).toFixed(2);
+        const outsOdds      = flopOrTurn === 'Flop' ? outsOddsFlop : outsOddsTurn;
 
         potOddsResult.value = potOdds + '%';
         equityResult.value  = outsOdds + '%';
@@ -30,10 +29,9 @@
 
     let resetButton = document.getElementsByClassName('reset')[0].addEventListener('click', () => {
         const inputs = document.getElementsByTagName('input');
-        for (var i = 0; i < inputs.length; i += 1) {
-            inputs[i].value = '';
-        }
-
-        goodBad.style.background = '#e8ebed';
+        for (var i = 0; i < inputs.length; i += 1) 
+            if (!(inputs[i].type === 'radio')) inputs[i].value = '';
+        document.querySelector('input[name="allIn"]').checked = false;
+        document.getElementsByClassName('goodBad')[0].style.background = '#e8ebed';
     });
 })();
